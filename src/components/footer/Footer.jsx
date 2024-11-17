add70e28aeffdaa5c66eecbed9d5d99723cbef6a
Aniket Pratap Singh <aniketpratapsingh7310@gmail.com>
2024-11-17 19:19:20 +0530

    footer Updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { FaInstagram } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaFacebookSquare } from "react-icons/fa";
-// import { CiTwitter } from "react-icons/ci";
-import { FaVimeo } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa6";
+
 import "./Footer.css";
-import fatsomabglogo from '../assests/fatsomabglogo.png'
+// import fatsomabglogo from '../assests/fatsomabglogo.png'
 
 import LowercaseEventsLogo from "../assests/LowercaseLogoRect.png";
 import { Link } from "react-router-dom";
@@ -18,114 +13,152 @@ const Footer = () => {
           <Link to="/">
             <img src={LowercaseEventsLogo} alt="" />
           </Link>
-          <p className="footer__beow_ogo_txt"></p>
+          <p className="footer__beow_ogo_txt">
+            CONTACT<span>@</span>LOWERCASEEVENTS.CO.UK <strong>|</strong>{" "}
+          </p>
         </div>
-        <div className="lisenced-note">
+        {/* <div className="lisenced-note">
           COPYRIGHT © 2024 Lower Case Events | Lower Case Events ltd. ALL RIGHTS
           RESERVED.
-        </div>
-      </div>
+        </div> */}
 
-      <div className="footer">
-        <div className="footer-box1">
-          <p className="footer-heading">LOWER CASE EVENTS</p>
-          <ul>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
+        <div className="footer">
+          <div className="footer-box1">
+
+            <ul>
 
             <li>
-              <Link to="/events">Events</Link>
-            </li>
+                <Link to="/about">Home</Link>
+              </li>
 
-            <li>
-              <Link to="/photos">Photos</Link>
-            </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
 
-            <li>
-              <Link to="/artist">Artist</Link>
-            </li>
+              <li>
+                <Link to="/events">Events</Link>
+              </li>
 
-            <li>
-              <Link to="/collab">Brands</Link>
-            </li>
-            <li>
-              <Link to="/">Contact Us</Link>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <Link to="/photos">Photos</Link>
+              </li>
 
-        <div className="footer-box4">
-          <p className="footer-heading">ENQUIRIES</p>
-          <ul>
-            <li>
-              <Link to="/">Carrers</Link>
-            </li>
-            <li>
-              <Link to="/">Partners</Link>
-            </li>
-            <li>
-              <Link to="/">University Partners</Link>
-            </li>
-            <li>
-              <Link to="/">Our Network</Link>
-            </li>{" "}
-            <li>
-              <Link to="/">University Partners</Link>
-            </li>{" "}
-            <li>
-              <Link to="/">FAQ's</Link>
-            </li>
-            <li>
-              <Link to="/">Playlist</Link>
-            </li>
-            <li>
-              <Link to="/">Our Story</Link>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <Link to="/artist">Artist</Link>
+              </li>
 
-        <div className="footer-box2">
-          <p className="footer-heading">ENQUIRIES</p>
-          <ul>
-            <li>info@lowercasegroup.co.uk</li>
-            <li>
-              <a
-                href="https://wa.me/447568564663"
-                target="_blank"
-                rel="noreferrer"
-              >
-                WhatsApp
-              </a>
-            </li>
-          </ul>
-        </div>
+              <li>
+                <Link to="/collab">Brands</Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className="footer-box3">
-          <div className="footer-heading">SOCIAL MEDIA</div>
-          <div className="social-icons">
-            <FaInstagram className="footer-icons" />
-            <FaWhatsapp className="footer-icons" />
-            <FaFacebookSquare className="footer-icons" />
-            {/* <CiTwitter className="footer-icons" /> */}
-            <FaYoutube className="footer-icons" />
+          <div className="footer-box4">
+     
+     <ul>
+       <li>
+         <Link to="/">Email us</Link>
+       </li>
+       <li>
+         <Link to="/">Group Bookings</Link>
+       </li>
+       <li>
+         <Link to="/">University Partners</Link>
+       </li>
+       <li>
+         <Link to="/">Birthdays</Link>
+       </li>{" "}
+       <li>
+         <Link to="/">Societies</Link>
+       </li>{" "}
+       <li>
+         <Link to="/">FAQ's</Link>
+       </li>
+       <li>
+         <Link to="/">Playlist</Link>
+       </li>
+       <li>
+         <Link to="/">Our Story</Link>
+       </li>
+     </ul>
+   </div>
 
-            <FaVimeo className="footer-icons" />
-            <img src={fatsomabglogo} alt="" />
+          <div className="footer-box4">
+     
+            <ul>
+              <li>
+                <Link to="/">Carrers</Link>
+              </li>
+              <li>
+                <Link to="/">Partners</Link>
+              </li>
+              <li>
+                <Link to="/">University Partners</Link>
+              </li>
+              <li>
+                <Link to="/">Our Network</Link>
+              </li>{" "}
+              <li>
+                <Link to="/">University Partners</Link>
+              </li>{" "}
+              <li>
+                <Link to="/">FAQ's</Link>
+              </li>
+            </ul>
+          </div>
 
+          <div className="footer-box2">
+
+          </div>
+
+          <div className="footer-box1">
+
+            <div className="social-icons">
+              <ul>
+                <li>
+                  <a href="/"> Instagam </a>
+                </li>
+
+                <li>
+                  <a            href="https://wa.me/447568564663"
+                  target="_blank"
+                  rel="noreferrer"> Whatsapp </a>
+                </li>
+
+                <li>
+                  <a href="/"> Facebook </a>
+                </li>
+
+                <li>
+                  <a href="/"> Fatsoma </a>
+                </li>
+
+                <li>
+                  <a href="/"> Youtube </a>
+                </li>
+
+                <li>
+                  <a href="/"> Vimeo </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
 
       <div className="bottom_footer">
+        <div className="lisenced-note">
+        <span> ©</span>   2024 LOWER CASE EVENTS, ALL RIGHTS RESERVED. WEB DEVELOPMENT <a href="/">LOWERCASE AGENCY</a>
+        </div>
 
         <ul>
           <li>
             <Link>Privacy Policy</Link>
+            <Link>|</Link>
+
             <Link>Terms & Conditions</Link>
           </li>
         </ul>
-        
       </div>
     </div>
   );
